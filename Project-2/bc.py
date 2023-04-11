73b692871e63d85449f936213fab66fec2dfcc39
Rushiraj <rushirajsinhherma7@gmail.com>
2023-04-10 21:39:20 -0400

    basic eval implemented

--- Project-2/bc.py
+++ Project-2/bc.py
@@ -1,18 +1,37 @@
 with open("input.txt") as file:
     lines = [line.rstrip() for line in file]
 
-    exp = {}
+exp = {}
 def assignmentFunction(line):
     line = line.replace(" ","")
     expression = line.split("=")
-    print(line)
+    # print(line)
     # print(expression[0], " = " , expression[1])
     exp[expression[0]] = expression[1]
-    print()
+    # print()
 for line in lines:
     # print(line)
     if("=" in line):
         assignmentFunction(line)
 
-print(exp)
+# print(exp)
     # print()
+
+# print(type(exp))
+
+variable = [exp.keys()] 
+print((variable))
+for keys in exp:
+    for var in variable :
+        for v in var:
+            if(v in exp[keys]):
+                # print(exp[keys],"hvfdjv")
+                exp[keys] = exp[keys].replace(v,exp[v])
+        
+
+
+for keys in exp:
+    print(exp[keys],"exp[key]") 
+    exp[keys] = eval(exp[keys])   
+
+print(exp)
