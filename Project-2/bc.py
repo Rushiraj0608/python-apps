3a6f3fe447150f54d00d9c71d4fdb86c1fcf728e
kavitha siratla <kavithasiratla@kavithas-MacBook-Pro.local>
2023-04-17 15:12:58 -0400

    parser for assignment

--- Project-2/bc.py
+++ Project-2/bc.py
@@ -73,6 +73,18 @@ def lex(s: str) -> list[token]:
             if word.isdigit():
                 tokens.append(token('int', word))
             i = end
+
+        elif s[i] == '_':
+            end = i + 1
+            while end < len(s) and (s[end].isalnum() or s[end] == '_'):
+                end += 1
+            assert end >= len(s) or not (s[end].isalnum() or s[end] == '_')
+
+            word = s[i:end]
+
+            tokens.append(token('var', word))
+            i = end
+
         elif s[i] == '(':
             tokens.append(token('sym', '('))
             i += 1
@@ -159,16 +171,135 @@ def lex(s: str) -> list[token]:
 
 # PARSING
 
+
 class ast():
     typ: str
     children: tuple[Any, ...]
 
     def __init__(self, typ: str, *children: Any):
+        """
+        x || true
+        >>> ast('||', ast('var', 'x'), ast('val', True))
+        ast('||', ast('var', 'x'), ast('val', True))
+        """
         self.typ = typ
         self.children = children
 
     def __repr__(self):
         return f'ast({self.typ!r}, {", ".join([repr(c) for c in self.children])})'
+
+def parse(s: str) -> ast:
+    ts = lex(s)
+
+    a, i = disj(ts, 0)
+
+    if i != len(ts):
+        raise SyntaxError(f"expected EOF, found {ts[i:]!r}")
+
+    return a
+
+def disj(ts: list[token], i: int) -> tuple[ast, int]:
+    """
+    >>> parse('true || false')
+    ast('||', ast('val', True), ast('val', False))
+    """
+    if i >= len(ts):
+        raise SyntaxError('expected conjunction, found EOF')
+
+    lhs, i = conj(ts, i)
+
+    while i < len(ts) and ts[i].typ == 'sym' and ts[i].val == '||':
+        rhs, i = conj(ts, i+1)
+        lhs = ast('||', lhs, rhs)
+
+    return lhs, i
+
+def conj(ts: list[token], i: int) -> tuple[ast, int]:
+    """
+    >>> parse('true && false')
+    ast('&&', ast('val', True), ast('val', False))
+    >>> parse('!x && (a && !false)')
+    ast('&&', ast('!', ast('var', 'x')), ast('&&', ast('var', 'a'), ast('!', ast('val', False))))
+    >>> parse('!x && a && !false')
+    ast('&&', ast('&&', ast('!', ast('var', 'x')), ast('var', 'a')), ast('!', ast('val', False)))
+    """
+    if i >= len(ts):
+        raise SyntaxError('expected conjunction, found EOF')
+
+    lhs, i = assign(ts, i)
+
+    while i < len(ts) and ts[i].typ == 'sym' and ts[i].val == '&&':
+        rhs, i = assign(ts, i+1)
+        lhs = ast('&&', lhs, rhs)
+
+    return lhs, i
+
+def assign(ts: list[token], i: int) -> tuple[ast, int]:
+    """
+    >>> parse('x = true')
+    ast('=', ast('var', 'x'), ast('val', True))
+    >>> parse('a = b = true')
+    ast('=', ast('var', 'a'), ast('=', ast('var', 'b'), ast('val', True)))
+    """
+
+    if i >= len(ts):
+        raise SyntaxError('expected assignment or disjunction, found EOF')
+
+    if ts[i].typ == 'var' and i+1 < len(ts) and ts[i+1].typ == 'asg' and ts[i+1].val == '=':
+        var_name = ts[i].val
+        rhs, i = neg(ts, i+2)
+        return ast('=', ast('var', var_name), rhs), i
+
+    return neg(ts, i)
+
+
+def neg(ts: list[token], i: int) -> tuple[ast, int]:
+    """
+    >>> parse('! true')
+    ast('!', ast('val', True))
+    >>> parse('!! true')
+    ast('!', ast('!', ast('val', True)))
+    """
+
+    if i >= len(ts):
+        raise SyntaxError('expected negation, found EOF')
+
+    if ts[i].typ == 'sym' and ts[i].val == '!':
+        a, i = neg(ts, i+1)
+        return ast('!', a), i
+    else:
+        return atom(ts, i)
+
+def atom(ts: list[token], i: int) -> tuple[ast, int]:
+    """
+    >>> parse('x')
+    ast('var', 'x')
+    >>> parse('true')
+    ast('val', True)
+    >>> parse('(((false)))')
+    ast('val', False)
+    """
+
+    t = ts[i]
+
+    if t.typ == 'var':
+        return ast('var', t.val), i+1
+    if t.typ == 'int':
+        return ast('int', t.val), i+1
+    elif t.typ == 'kw' and t.val in ['true', 'false']:
+        return ast('val', t.val == 'true'), i + 1
+    elif t.typ == 'sym' and t.val == '(':
+        a, i = disj(ts, i + 1)
+
+        if i >= len(ts):
+            raise SyntaxError(f'expected right paren, got EOF')
+
+        if not (ts[i].typ == 'sym' and ts[i].val == ')'):
+            raise SyntaxError(f'expected right paren, got "{ts[i]}"')
+        
+        return a, i + 1
+
+    raise SyntaxError(f'expected atom, got "{ts[i]}"')
 
 # INTERPRETER
 
@@ -229,4 +360,28 @@ def interp(a: ast, env: set[str]) -> bool:
         
     except ZeroDivisionError:
         return 'divide by zero'
+    
 
+# with open("input.txt") as file:
+#     lines = [line.rstrip() for line in file]
+
+# for i in lines:
+#     ast = parse(i)
+
+
+# expr = 'true || false && !x'
+expr = 'x=3'
+ast = parse(expr)
+print(ast)
+# print(ast.children[0])
+# print(ast.children[0].children[0])
+# print(ast.children[1]) 
+# print(ast.children[1].children[0])  
+
+# expr = 'true || false && !x'
+# ast = parse(expr)
+# print(ast.typ)
+# print(ast.children[0].typ)
+# print(ast.children[0].children[0])
+# print(ast.children[1].typ)
+# print(ast.children[1].children[0].typ) 
